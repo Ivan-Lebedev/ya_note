4f1960aca6cb345bcfd60ce4671f7ff1bf84f3f5
Ivan Lebedev <ivan_lbd@mail.ru>
2024-02-25 22:55:14 +0300

    init Django pytest lesson 1

---
+++ notes/pytest_tests/test_routes.py
@@ -0,0 +1,26 @@
+import pytest
+from http import HTTPStatus
+
+from django.urls import reverse
+
+
+@pytest.mark.parametrize(
+    'name',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+)
+# Указываем имя изменяемого параметра в сигнатуре теста.
+def test_pages_availability_for_anonymous_user(client, name):
+    url = reverse(name)  # Получаем ссылку на нужный адрес.
+    response = client.get(url)  # Выполняем запрос.
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:list', 'notes:add', 'notes:success')
+)
+def test_pages_availability_for_auth_user(not_author_client, name):
+    url = reverse(name)
+    response = not_author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
