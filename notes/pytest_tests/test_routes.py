2c875d599fd840d628f142e23d9ce6f8e2967f66
Иван Лебедев <ivan_lbd@mail.ru>
2024-02-26 15:45:09 +0300

    Django pytest lesson 4

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -24,3 +24,24 @@ def test_pages_availability_for_auth_user(not_author_client, name):
     url = reverse(name)
     response = not_author_client.get(url)
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    # Предварительно оборачиваем имена фикстур
+    # в вызов функции pytest.lazy_fixture().
+    (
+        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_different_users(
+        parametrized_client, name, note, expected_status
+):
+    url = reverse(name, args=(note.slug,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
